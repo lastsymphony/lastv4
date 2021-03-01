920e54bdbce1868c5855f9a76d62a6c43460d3ab
lastsymphony <78311812+lastsymphony@users.noreply.github.com>
2021-03-01 22:18:57 +0700

    Add files via upload

---
+++ plugins/creator.js
@@ -0,0 +1,19 @@
+let handler = function (m) {
+  // this.sendContact(m.chat, '6281515860089', 'Nurutomo', m)
+ this.sendContact(m.chat, '6289670394574', 'Aditya jatayu', m),
+ this.sendContact(m.chat, '6285747874973', 'Kemal Ibnu', m),
+ this.sendContact(m.chat, '6285823554146', '》Wilka_Kenap《', m),
+ this.sendContact(m.chat, '6285762005157', 'Trian', m)
+ this.sendContact(m.chat, '6281319352377', 'lord', m)
+ this.sendContact(m.chat, '6282273025238', '『HIRO』༺½༻', m)
+ this.sendContact(m.chat, '6285240424044', 'Mahesa :v', m)
+ this.sendContact(m.chat, '6289505289896', 'Calvin', m)
+ this.sendContact(m.chat, '62895433996700', 'Odin/Hashira', m)
+  this.sendContact(m.chat, '62895618689182', 'Ade', m)
+}
+handler.help = ['owner', 'creator']
+handler.tags = ['info']
+
+handler.command = /^(owner|creator)$/i
+
+module.exports = handler
