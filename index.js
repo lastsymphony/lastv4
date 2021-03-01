50670b184d09e77c051ba54114cb0349e24b78fb
lastsymphony <78311812+lastsymphony@users.noreply.github.com>
2021-03-01 22:10:36 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,50 @@
+console.log('Starting...')
+let { spawn } = require('child_process')
+let path = require('path')
+let fs = require('fs')
+let package = require('./package.json')
+const CFonts  = require('cfonts')
+CFonts.say('AdityaJatayu\nSymphony Bot', {
+  font: 'chrome',
+  align: 'center',
+  gradient: ['cyan', 'cyan']
+})
+CFonts.say(`'${package.name}' By @${package.author.name || package.author}`, {
+  font: 'console',
+  align: 'center',
+  gradient: ['cyan', 'cyan']
+})
+
+function start(file) {
+  let args = [path.join(__dirname, file), ...process.argv.slice(2)]
+  CFonts.say([process.argv[0], ...args].join(' '), {
+    font: 'console',
+    align: 'center',
+    gradient: ['cyan', 'cyan']
+  })
+  let p = spawn(process.argv[0], args, {
+    stdio: ['inherit', 'inherit', 'inherit', 'ipc']
+  })
+  .on('message', data => {
+    console.log('[RECEIVED]', data)
+    switch (data) {
+      case 'reset':
+        p.kill()
+        start.apply(this, arguments)
+        break
+      case 'uptime':
+        p.send(process.uptime())
+        break
+    }
+  })
+  .on('error', e => {
+    console.error(e)
+    fs.watchFile(args[0], () => {
+      start()
+      fs.unwatchFile(args[0])
+    })
+  })
+  // console.log(p)
+}
+
+start('main.js')
